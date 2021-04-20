d6a7955d9d82c7f10fe12bef8ff42501646ca98a
Rodrigo Carvalho Moraes <rodrigomoraes61@outlook.com>
2021-04-20 11:15:44 -0300

    In the Home - Created Level Button and Quiz Card and Modify Score Card, Created Challeng Page with Widgets (Awnser, Question Indicator, Quiz), Created Shared folder to Widget Progress Indicator, Modify App_Text_Styles to bold mediun

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:dev_quizz_nlw/home/widgets/appbar/app_bar_widget.dart';
+import 'package:dev_quizz_nlw/home/widgets/level_button/level_button_widget.dart';
+import 'package:dev_quizz_nlw/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}): super(key:key);
+  HomePage({Key? key}) : super(key: key);
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -12,6 +14,43 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: "Fácil"),
+                LevelButtonWidget(label: "Médio"),
+                LevelButtonWidget(label: "Difícil"),
+                LevelButtonWidget(label: "Perito"),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Expanded(
+              child: GridView.count(
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            crossAxisCount: 2,
+            children: [
+              QuizCardWidget(),
+              QuizCardWidget(),
+              QuizCardWidget(),
+              QuizCardWidget(),
+              QuizCardWidget(),
+              QuizCardWidget(),
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
